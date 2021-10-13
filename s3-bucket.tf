725502519d3ede2acd7e65ff0e0171f63e090a3d
josna battula <sudhakargroup@hotmail.com>
2021-10-13 05:55:35 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,6 @@
 module "s3-bucket" {
   source  = "app.terraform.io/josna-training/s3-bucket/aws"
-  version = "2.9.0"
+  version = "2.2.0"
   # insert required variables here
   bucket_prefix ="${var.prefix}"
 }
